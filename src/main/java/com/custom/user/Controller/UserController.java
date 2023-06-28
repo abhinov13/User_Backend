cf434ee28762c5e81e3490ceebc80aec45762c01
loronto <46075089+loronto@users.noreply.github.com>
2023-06-28 15:24:08 +0530

    Trying Docker

--- src/main/java/com/custom/user/Controller/UserController.java
+++ src/main/java/com/custom/user/Controller/UserController.java
@@ -1,10 +1,8 @@
 package com.custom.user.Controller;
 
-import java.beans.JavaBean;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
